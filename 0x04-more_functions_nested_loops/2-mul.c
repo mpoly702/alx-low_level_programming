a0b792254fd54af65dce09d86b354efc66e0122a
Godfrey <mpoly702@gmail.com>
2023-08-17 13:32:20 +0100

    gill

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  mul - a function that multiplies two integers 
+ *  mul - a function that multiplies two integers
  *  @a: first input
  *  @b: second input
  *  Return: multiplication result of the 2 inputs
