46b0044e947e64c23b0a7bfe700a5d5ce6c7bd2d
Godfrey <mpoly702@gmail.com>
2023-08-17 17:55:32 +0100

    gbg

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,6 @@
  * Return: 0-9, excluding 2 and 4, followed by new line
  */
 
-/**
- * print_most_numbers - a function that prints the numbers, from 0 to 9
- *
- * Return: 0-9, excluding 2 and 4, followed by new line
- */
-
 void print_most_numbers(void)
 {
 	int i;
